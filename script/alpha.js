1aa7218d7387d95380fc12ef346ce97cf4f04f7b
MdJowelAhmed <155435567+MdJowelAhmed@users.noreply.github.com>
2024-02-15 18:58:11 +0600

    finish this game

--- script/alpha.js
+++ script/alpha.js
@@ -14,6 +14,10 @@ function handleKeyupPress(event){
     const playerPress=event.key;
     console.log('Player press',playerPress)
 
+    if(playerPress==='Escape'){
+        gameIsOver();
+    }
+
     // expected press 
     const alphabetElement=document.getElementById('current-alphabet');
     const currentAlphabet=alphabetElement.innerText.toLowerCase();
@@ -22,22 +26,43 @@ function handleKeyupPress(event){
     // check macth 
     if(currentAlphabet===playerPress){
         console.log('You win and get point 1');
+
+        const currentScoreDisplay=elementTextId('current-score')
+        console.log(currentScoreDisplay);
+        const updateScore=currentScoreDisplay + 1;
+        setElementId('current-score',updateScore)
         
         // update score 
-        const currentScoreElement=document.getElementById('current-score');
-        const currentScoreInner=currentScoreElement.innerText;
-        const currentScore=parseInt(currentScoreInner);
-        console.log(currentScore);
+        // const currentScoreElement=document.getElementById('current-score');
+        // const currentScoreInner=currentScoreElement.innerText;
+        // const currentScore=parseInt(currentScoreInner);
+        // console.log(currentScore);
 
-        const newScore=currentScore + 1;
+        // const newScore=currentScore + 1;
 
-        currentScoreElement.innerText=newScore
+        // currentScoreElement.innerText=newScore
+
         console.log(currentAlphabet);
         removeBackgroundColor(currentAlphabet);
         gameContinue()
     }
     else{
         console.log('You wrong so you lost your point and try again mode successful by self')
+        const currentLife=elementTextId('current-life');
+        const updatedLife=currentLife - 1;
+        setElementId('current-life',updatedLife)
+
+        if(updatedLife ===0){
+            gameIsOver()
+        }
+
+
+        // const currentLifeElement=document.getElementById('current-life');
+        // const currentLifeInner=currentLifeElement.innerText;
+        // const currentLife=parseInt(currentLifeInner);
+        // console.log(currentLife)
+        // const newLife=currentLife - 1;
+        // currentLifeElement.innerText = newLife;
     }
 
 }
@@ -54,6 +79,22 @@ function gameContinue(){
 
 function play(){
     hideElementId('home-screen');
-    showGetElement('play-ground')
+    showGetElement('play-ground');
+    hideElementId('final-score');
+
+    setElementId('current-life',5);
+    setElementId('current-score',0)
     gameContinue();
+}
+
+function gameIsOver(){
+    hideElementId('play-ground')
+    showGetElement('final-score')
+
+    const lastScore=elementTextId('current-score');
+    console.log(lastScore)
+    setElementId('last-score',lastScore);
+
+    const currentAlphabet=getElementTextById('current-alphabet');
+    removeBackgroundColor(currentAlphabet)
 }
--- script/function.js
+++ script/function.js
@@ -17,6 +17,21 @@ function removeBackgroundColor(elementId){
     const bgColor=document.getElementById(elementId);
     bgColor.classList.remove('bg-orange-400')
 }
+function elementTextId(elementId){
+    const currentElementScore=document.getElementById(elementId);
+    const currentElementValue=currentElementScore.innerText;
+    const currentScore=parseInt(currentElementValue);
+    return currentScore
+}
+function setElementId(elementId,value){
+    const element=document.getElementById(elementId);
+    element.innerHTML=value
+}
+function getElementTextById(elementId){
+    const element=document.getElementById(elementId);
+    const text=element.innerText;
+    return text
+}
 
 function getAlphabetRandomNumber(){
     const alphabetRandom="abcdefghijklmnopqrstuvwxyz"
